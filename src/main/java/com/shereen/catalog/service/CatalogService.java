571da59648ce6a384ba4cb53e5f15576c1e09097
shereenhamato <shereen.hamato@gmail.com>
2019-01-07 11:41:58 +0800

    feat: add catalog service

---
+++ src/main/java/com/shereen/catalog/service/CatalogService.java
@@ -0,0 +1,23 @@
+package com.shereen.catalog.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.shereen.catalog.model.Catalog;
+
+@Service
+public class CatalogService {
+	
+	private static List<Catalog> catalogs= new ArrayList<>();
+	
+	
+
+	static {
+		for (int i =0 ; i<= 10; i++) {
+			Catalog cat = new Catalog(i , "catalog "+i , "catalog "+i+" desc");
+			catalogs.add(cat);
+		}
+	}
+}
