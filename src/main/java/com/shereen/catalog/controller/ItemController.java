873dd4e0cb10924d471a12ef1b055c57402b55c0
shereenhamato <shereen.hamato@gmail.com>
2019-01-08 14:02:15 +0800

    feat: add item controller

---
+++ src/main/java/com/shereen/catalog/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.shereen.catalog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shereen.catalog.service.ItemService;
+
+@Controller
+@RequestMapping("/Items")
+public class ItemController {
+	
+	@Autowired
+	ItemService itemService;
+	
+	@GetMapping
+	public String getAllItems(Model model) {
+		model.addAttribute("items", itemService);
+		return "item-list";
+	}
+	
+
+}
