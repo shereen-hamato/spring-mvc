87032656f27e0247d1d725e5417c2c1130efd59c
shereenhamato <shereen.hamato@gmail.com>
2019-01-08 16:59:01 +0800

    feat: add JSON response for item listing

--- src/main/java/com/shereen/catalog/controller/ItemController.java
+++ src/main/java/com/shereen/catalog/controller/ItemController.java
@@ -1,12 +1,16 @@
 package com.shereen.catalog.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.shereen.catalog.model.Item;
 import com.shereen.catalog.service.ItemService;
 
 @Controller
@@ -20,6 +24,12 @@ public class ItemController {
 	public String getAllItems(Model model, @PathVariable long cataId) {
 		model.addAttribute("items", itemService.getAllItems( cataId));
 		return "item-list";
+	}
+	
+	@GetMapping("{cataId}/items/JSON")
+	@ResponseBody()
+	public List<Item> getAllItemsJson(@PathVariable long cataId){
+		return itemService.getAllItems( cataId);
 	}
 	
 
