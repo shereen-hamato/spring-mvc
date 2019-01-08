96deb5fe399677e5a6fcdc87b03c491857859766
shereenhamato <shereen.hamato@gmail.com>
2019-01-08 16:49:38 +0800

    feat: add JSON response for catalog

--- src/main/java/com/shereen/catalog/controller/CatalogController.java
+++ src/main/java/com/shereen/catalog/controller/CatalogController.java
@@ -1,11 +1,15 @@
 package com.shereen.catalog.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.shereen.catalog.model.Catalog;
 import com.shereen.catalog.service.CatalogService;
 
 @Controller
@@ -22,5 +26,10 @@ public class CatalogController {
 	}
 	
 	
+	@GetMapping("/JSON")
+	@ResponseBody
+	public List<Catalog> getAllCatalogJson(){
+		return cataService.getAllCatalogs();
+	}
 
 }
