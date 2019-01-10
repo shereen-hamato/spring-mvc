690d5473aba1ba365c0cdc1cb1f312299e9cf6cd
shereenhamato <shereen.hamato@gmail.com>
2019-01-10 16:23:36 +0800

    feat: add function to catalog service

--- src/main/java/com/shereen/catalog/repository/CatalogRepository.java
+++ src/main/java/com/shereen/catalog/repository/CatalogRepository.java
@@ -1,8 +1,6 @@
 package com.shereen.catalog.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.shereen.catalog.model.Catalog;
--- src/main/java/com/shereen/catalog/service/CatalogService.java
+++ src/main/java/com/shereen/catalog/service/CatalogService.java
@@ -5,28 +5,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.shereen.catalog.model.Catalog;
 import com.shereen.catalog.repository.CatalogRepository;
 
 @Service
+@Transactional
 public class CatalogService {
-	
-	private static List<Catalog> catalogs= new ArrayList<>();
-	
+
 	@Autowired
 	CatalogRepository cataRepo;
 
-	static {
-		for (int i =1 ; i<= 10; i++) {
-			Catalog cat = new Catalog(i , "catalog "+i , "catalog "+i+" desc");
-			catalogs.add(cat);
-		}
-	}
-	
-	public List<Catalog> getAllCatalogs(){
+	public List<Catalog> getAllCatalogs() {
 		List<Catalog> cats = new ArrayList<>();
-		cataRepo.findAll().forEach(cat->cats.add(cat));
+		cataRepo.findAll().forEach(cat -> cats.add(cat));
 		return cats;
+	}
+
+	public Catalog getCatalogById(long id) {
+		// TODO: handle the null exception
+		return cataRepo.findById(id).get();
+	}
+
+	public Catalog addCatalog(Catalog cata) {
+		return cataRepo.save(cata);
+	}
+
+	public Catalog updateCatalg(Catalog cata) {
+		return cataRepo.save(cata);
+	}
+
+	public void deleteCatalog(long id) {
+		cataRepo.deleteById(id);
 	}
 }
