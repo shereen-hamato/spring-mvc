4ee17eea23a4b2669d82cca62afbe73cabcd6ac8
shereenhamato <shereen.hamato@gmail.com>
2019-01-10 15:18:32 +0800

    feat: add database schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE ITEM_CATALOG (
+id INTEGER  PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) ,
+description VARCHAR (255)
+);
+
+CREATE TABLE ITEM (
+id int PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR (255),
+description VARCHAR (255),
+price DOUBLE ,
+duration VARCHAR (255),
+item_catalog_id int,
+foreign key (item_catalog_id) references ITEM_CATALOG(id)
+);
