a96f84fa9a0db00cef3f762d7d54d54d9581a971
shereenhamato <shereen.hamato@gmail.com>
2019-01-10 15:18:01 +0800

    feat: insert data into data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO item_catalog ( name , description) VALUES('books', 'books of different gerene and authors');
+INSERT INTO item_catalog ( name , description) VALUES('accessories', 'accessories for different designers');
+INSERT INTO item_catalog ( name , description) VALUES('cars', 'cars for differeny brands');
+INSERT INTO item_catalog ( name , description) VALUES('furnture', 'furnture for different designers');
+
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('book 1', 'book 1 description', 1.2, '2 month', '1');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('book 2', 'book 2 description', 1.2, '3 month', '1');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('accessories 1', 'accessories 1 description', 1.5, '1 month', '2');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('accessories 2', 'accessories 2 description', 1.7, '5 month', '2');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('cars 1', 'cars 1 description', 1.2, '3 month', '3');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('cars 2', 'cars 2 description', 1.5, '1 month', '3');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('furnture 1', 'furnture 1 description', 1.2, '3 month', '4');
+INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('furnture 2', 'furnture 2 description', 1.5, '1 month', '4');
