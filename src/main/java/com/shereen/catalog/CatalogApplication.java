04823cccb69c7afc2757c43254180b6859307bd5
shereenhamato <shereen.hamato@gmail.com>
2019-01-30 22:13:07 +0800

    feat: add error controller

--- src/main/java/com/shereen/catalog/CatalogApplication.java
+++ src/main/java/com/shereen/catalog/CatalogApplication.java
@@ -1,9 +1,12 @@
 package com.shereen.catalog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class CatalogApplication {
 
 	public static void main(String[] args) {
