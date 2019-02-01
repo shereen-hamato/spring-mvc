ecee8b574850210f7856b857b607eb92eb1ef0cc
shereenhamato <shereen.hamato@gmail.com>
2019-02-02 00:45:21 +0800

    fix: return optional from get catalog by id

--- src/main/java/com/shereen/catalog/service/CatalogService.java
+++ src/main/java/com/shereen/catalog/service/CatalogService.java
@@ -2,6 +2,7 @@ package com.shereen.catalog.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,9 +24,8 @@ public class CatalogService {
 		return cats;
 	}
 
-	public Catalog getCatalogById(long id) {
-		// TODO: handle the null exception
-		return cataRepo.findById(id).get();
+	public Optional<Catalog> getCatalogById(long id) {
+		return cataRepo.findById(id);
 	}
 
 	public Catalog addCatalog(Catalog cata) {
