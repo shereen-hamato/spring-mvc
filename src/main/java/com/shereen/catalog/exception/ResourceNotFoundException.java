c72aaece95fbc4e079b98e01f73104bed915b435
shereenhamato <shereen.hamato@gmail.com>
2019-02-01 20:32:49 +0800

    feat : add custom exception classes

--- src/main/java/com/shereen/catalog/exception/ResourceNotFoundException.java
+++ src/main/java/com/shereen/catalog/exception/ResourceNotFoundException.java
@@ -1,5 +1,8 @@
 package com.shereen.catalog.exception;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ResourceNotFoundException extends RuntimeException {
 
 	/**
--- src/main/java/com/shereen/catalog/exception/ValidationException.java
+++ src/main/java/com/shereen/catalog/exception/ValidationException.java
@@ -1,5 +1,8 @@
 package com.shereen.catalog.exception;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ValidationException extends RuntimeException {
 
 	/**
