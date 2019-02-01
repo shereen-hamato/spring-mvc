a97ffed16bdf0f39656f1458148064959171bf4c
shereenhamato <shereen.hamato@gmail.com>
2019-02-01 17:31:32 +0800

    feat: add custom exceptions

---
+++ src/main/java/com/shereen/catalog/exception/ValidationException.java
@@ -0,0 +1,10 @@
+package com.shereen.catalog.exception;
+
+public class ValidationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6503071239719432709L;
+
+}
