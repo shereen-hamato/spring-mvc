9108961213ffe8378ab44da1ae16969e894c2c3b
shereenhamato <shereen.hamato@gmail.com>
2019-02-05 17:30:42 +0800

    feat: display image in catlaog list view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO item_catalog ( name , description) VALUES('books', 'books of different gerene and authors');
-INSERT INTO item_catalog ( name , description) VALUES('accessories', 'accessories for different designers');
-INSERT INTO item_catalog ( name , description) VALUES('cars', 'cars for differeny brands');
-INSERT INTO item_catalog ( name , description) VALUES('furnture', 'furnture for different designers');
+INSERT INTO item_catalog ( name , description, image_path) VALUES('books', 'books of different gerene and authors', 'https://imgplaceholder.com/640x360');
+INSERT INTO item_catalog ( name , description , image_path) VALUES('accessories', 'accessories for different designers', 'https://imgplaceholder.com/640x360');
+INSERT INTO item_catalog ( name , description, image_path) VALUES('cars', 'cars for differeny brands', 'https://imgplaceholder.com/640x360');
+INSERT INTO item_catalog ( name , description, image_path) VALUES('furnture', 'furnture for different designers', 'https://imgplaceholder.com/640x360');
 
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('book 1', 'book 1 description', 1.2, '2 month', '1');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('book 2', 'book 2 description', 1.2, '3 month', '1');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('accessories 1', 'accessories 1 description', 1.5, '1 month', '2');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('accessories 2', 'accessories 2 description', 1.7, '5 month', '2');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('cars 1', 'cars 1 description', 1.2, '3 month', '3');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('cars 2', 'cars 2 description', 1.5, '1 month', '3');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('furnture 1', 'furnture 1 description', 1.2, '3 month', '4');
-INSERT INTO item (name, description, price, duration, item_catalog_id) VALUES ('furnture 2', 'furnture 2 description', 1.5, '1 month', '4');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('book 1', 'book 1 description', 1.2, '2 month', '1', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('book 2', 'book 2 description', 1.2, '3 month', '1', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('accessories 1', 'accessories 1 description', 1.5, '1 month', '2', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('accessories 2', 'accessories 2 description', 1.7, '5 month', '2', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('cars 1', 'cars 1 description', 1.2, '3 month', '3', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('cars 2', 'cars 2 description', 1.5, '1 month', '3', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('furnture 1', 'furnture 1 description', 1.2, '3 month', '4', 'https://via.placeholder.com/100/09f/fff.png');
+INSERT INTO item (name, description, price, duration, item_catalog_id, image_path) VALUES ('furnture 2', 'furnture 2 description', 1.5, '1 month', '4', 'https://via.placeholder.com/100/09f/fff.png');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE ITEM_CATALOG (
 id INTEGER  PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) ,
-description VARCHAR (255)
+description VARCHAR (255),
+image_path VARCHAR (255)
 );
 
 CREATE TABLE ITEM (
@@ -11,5 +12,6 @@ description VARCHAR (255),
 price DOUBLE ,
 duration VARCHAR (255),
 item_catalog_id int,
+image_path VARCHAR (255),
 foreign key (item_catalog_id) references ITEM_CATALOG(id)
 );
