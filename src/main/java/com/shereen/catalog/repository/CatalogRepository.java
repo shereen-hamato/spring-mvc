9b222c2a15b2c3694d9a993dd0eb05a2084d58ad
shereenhamato <shereen.hamato@gmail.com>
2019-02-12 16:13:45 +0800

    feat: add pagenated catalog repo

--- src/main/java/com/shereen/catalog/repository/CatalogRepository.java
+++ src/main/java/com/shereen/catalog/repository/CatalogRepository.java
@@ -1,12 +1,12 @@
 package com.shereen.catalog.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shereen.catalog.model.Catalog;
 
 @Repository
-public interface CatalogRepository extends CrudRepository<Catalog, Long> {
+public interface CatalogRepository extends PagingAndSortingRepository<Catalog, Long> {
 	
 	
 	
