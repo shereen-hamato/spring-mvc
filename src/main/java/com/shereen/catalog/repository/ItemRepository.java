6437f8e79bdfc2f5224bcf3a35b87fb70bfc3543
shereenhamato <shereen.hamato@gmail.com>
2019-02-14 23:52:17 +0800

    feat: change to pagenation repo

--- src/main/java/com/shereen/catalog/repository/ItemRepository.java
+++ src/main/java/com/shereen/catalog/repository/ItemRepository.java
@@ -2,16 +2,18 @@ package com.shereen.catalog.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shereen.catalog.model.Catalog;
 import com.shereen.catalog.model.Item;
 
 @Repository
-public interface ItemRepository extends CrudRepository<Item, Long> {
+public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
 	
-	public Iterable<Item> findAllByCatalog(Catalog catalog);
+	public Page<Item> findAllByCatalog(Pageable page,Catalog catalog);
 	
 	public Optional<Item> findByIdAndCatalog(Long id, Catalog catalog);
 
